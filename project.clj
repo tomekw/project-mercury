4cdc58747bb55ea078f514f3a5cad03e62e9bdc7
Tomek Wałkuski <ja@jestem.tw>
2014-09-14 20:07:03 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]
+                 [compojure "1.1.9"]
                  [clojurewerkz/route-one "1.1.0"]
-                 [liberator "0.12.0"]
+                 [liberator "0.12.1"]
                  [postgresql "9.3-1102.jdbc41"]
                  [org.clojure/tools.logging "0.3.0"]
-                 [puppetlabs/trapperkeeper "0.4.3"]
-                 [puppetlabs/trapperkeeper-webserver-jetty9 "0.5.2"]]
+                 [puppetlabs/trapperkeeper "0.5.1"]
+                 [puppetlabs/trapperkeeper-webserver-jetty9 "0.7.5"]]
   :aliases {"tk" ["trampoline" "run" "--config" "resources/config/config.conf" "--bootstrap-config" "resources/config/bootstrap.cfg"]}
   :main puppetlabs.trapperkeeper.main)
