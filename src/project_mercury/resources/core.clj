97a9ba7b49d7cff6afb041966ff1a0649451776c
Tomek Wałkuski <ja@jestem.tw>
2014-09-21 20:15:08 +0200

    More middlewares

--- src/project_mercury/resources/core.clj
+++ src/project_mercury/resources/core.clj
@@ -1,8 +1,13 @@
 (ns project-mercury.resources.core
   (:require [bidi.bidi :as routes]
+            [ring.middleware.secure-headers :refer [wrap-secure-headers]]
             [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
             [ring.middleware.session :refer [wrap-session]]
-            [ring.middleware.secure-headers :refer [wrap-secure-headers]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.nested-params :refer [wrap-nested-params]]
+            [ring.middleware.multipart-params :refer [wrap-multipart-params]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.cookies :refer [wrap-cookies]]
             [clojure.tools.logging :as log]
             [project-mercury.resources.users :as users]))
 
@@ -20,4 +25,9 @@
     (routes/make-handler (make-routes (make-handlers datasource)))
     wrap-secure-headers
     wrap-anti-forgery
-    wrap-session))
+    wrap-session
+    wrap-keyword-params
+    wrap-nested-params
+    wrap-multipart-params
+    wrap-params
+    wrap-cookies))
