281c44647f361e746fb7a6024fd2dea3f1399490
Tomek Wałkuski <ja@jestem.tw>
2014-09-26 18:40:15 +0200

    Working migration service

--- src/jupiter/core.clj
+++ src/jupiter/core.clj
@@ -4,56 +4,58 @@
             [clojure.java.jdbc :as jdbc]))
 
 (def ^{:private true} schema-migrations-table-ddl
-  ["CREATE TABLE IF NOT EXISTS schema_migrations (version text NOT NULL);
-   CREATE UNIQUE INDEX index_schema_migrations_on_version
-   ON schema_migrations (version);"])
+  ["CREATE TABLE IF NOT EXISTS schema_migrations (version text NOT NULL)"
+   "CREATE UNIQUE INDEX index_schema_migrations_on_version ON schema_migrations (version)"])
+
+(def ^{:private true} applied-versions-query
+  ["SELECT version FROM schema_migrations"])
 
 (def ^{:private true} migration-pattern
-  #"\A\d+_.+\.sql\z")
+  #"\A(\d+)_(.+)\.sql\z")
 
 (defn- migration-file? [file]
   (re-matches migration-pattern (.getName file)))
 
+(defn- migration [migration-file]
+  (let [[_ version name] (re-find migration-pattern (.getName migration-file))]
+    {:file    migration-file
+     :version version
+     :name    name}))
+
 (defn- all-migrations [migrations-path]
-  (filter migration-file? (.listFiles (clojure.java.io/file migrations-path))))
+  (map #(migration %)
+       (filter migration-file?
+               (.listFiles (clojure.java.io/file migrations-path)))))
 
-(defn- migration-id [migration]
-  (first (clojure.string/split (.getName migration) #"_")))
-
-(defn- migration-ids [migrations]
-  (set (map migration-id migrations)))
-
-(defn- all-migration-ids [migrations-path]
-  (migration-ids (all-migrations migrations-path)))
-
-(defn- applied-migration-ids [datasource]
-  (set (map :version (jdbc/query datasource
-                                 ["SELECT version FROM schema_migrations"]))))
-
-(defn- pending-migration-ids [datasource migrations-path]
-  (clojure.set/difference (all-migration-ids migrations-path)
-                          (applied-migration-ids datasource)))
-
-(defn- pending-migrations [datasource migrations-path]
-  (filter #((pending-migration-ids datasource migrations-path) (migration-id %)) (all-migrations migrations-path)))
+(defn- pending-migrations [datasource all-migrations]
+  (let [applied-versions (set (jdbc/query datasource applied-versions-query))]
+    (filter #(not (applied-versions (select-keys % [:version]))) all-migrations)))
 
 (defn- migration-body [migration]
-  [(slurp migration)])
+  [(slurp (:file migration))])
 
-(defn- apply-migration [datasource migration-body]
+(defn- register-migration-query [migration]
+  ["INSERT INTO schema_migrations (version) VALUES (?)" (:version migration)])
+
+(defn- apply-migration [datasource migration]
+  (log/info (str "Applying migration " (:name migration) "..."))
   (try
-    (jdbc/execute! datasource migration-body)
+    (jdbc/with-db-transaction [conn datasource]
+      (jdbc/execute! conn (register-migration-query migration))
+      (jdbc/execute! conn (migration-body migration)))
     (catch java.sql.SQLException e
       (log/error (.getMessage e))
       (throw e))))
 
 (defn- apply-migrations [datasource migrations]
-  (doall (map #(apply-migration datasource (migration-body %)) migrations)))
+  (doall (map #(apply-migration datasource %) migrations)))
 
 (defn ensure-schema-migrations-table [datasource]
   (try
-    (jdbc/execute! datasource schema-migrations-table-ddl)
+    (apply jdbc/db-do-commands datasource schema-migrations-table-ddl)
     (catch java.sql.SQLException _)))
 
 (defn migrate [datasource migrations-path]
-  (apply-migrations datasource (pending-migrations datasource migrations-path)))
+  (apply-migrations datasource
+                    (pending-migrations datasource
+                                        (all-migrations migrations-path))))
