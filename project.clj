381b7163ff830757ea373f7e2b72572ada646ec7
Tomek Wałkuski <ja@jestem.tw>
2014-10-03 22:05:55 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[bidi "1.10.5"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/java.jdbc "0.3.5"]
-                 [hikari-cp "0.6.0"]
+                 [hikari-cp "0.7.0"]
                  [liberator "0.12.2"]
                  [postgresql "9.3-1102.jdbc41"]
                  [prone "0.6.0"]
@@ -14,7 +14,7 @@
                  [dhruv/ring-secure-headers "0.3.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [puppetlabs/trapperkeeper "0.5.1"]
-                 [puppetlabs/trapperkeeper-webserver-jetty9 "0.7.5"]
+                 [puppetlabs/trapperkeeper-webserver-jetty9 "0.7.7"]
                  [yesql "0.4.0"]]
   :aliases {"tk" ["trampoline" "run" "--config" "resources/config/config.edn" "--bootstrap-config" "resources/config/bootstrap.cfg"]}
   :main puppetlabs.trapperkeeper.main)
