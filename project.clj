0809c820370cb3f8679f00484cb35659452263fd
Tomek Wałkuski <ja@jestem.tw>
2014-10-30 14:35:37 +0100

    Update java.jdbc

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[bidi "1.10.5"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.clojure/java.jdbc "0.3.6"]
                  [hikari-cp "0.8.0"]
                  [liberator "0.12.2"]
                  [postgresql "9.3-1102.jdbc41"]
